bffd6d83c14f41f11f4985015b3e9712f32d6de4
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2017-03-31 00:23:43 -0300

    Salida por consola

--- src/adivinanza.cpp
+++ src/adivinanza.cpp
@@ -1,4 +1,6 @@
 
+#include <iostream>
+
 /*
  * Programa que le ofrece jugar al usuario
  * a adivinar un valor entero comprendido entre
@@ -9,6 +11,12 @@ int main() {
 	int numeroArriesgado;
 
 	const int numeroSecreto = 7;
+
+	std::cout << "Bienvenido !!!"
+	          << std::endl
+			  << "Estoy pensando en un numero entre 0 y 99"
+			  << std::endl;
+
 
 	return 0;
 }
